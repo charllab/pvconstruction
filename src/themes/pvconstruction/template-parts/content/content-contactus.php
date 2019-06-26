948782819d9483a90c04c6c1e88ac49dace22856
charllab <developer.charl@gmail.com>
2019-06-26 10:38:31 -0700

    form layout and styling tweaks

--- src/themes/pvconstruction/template-parts/content/content-contactus.php
+++ src/themes/pvconstruction/template-parts/content/content-contactus.php
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="row justify-content-between">
-        <div class="col-lg-6 col-xl-5">
+        <div class="col-xl-5">
             <h2 class="text-capitalize">Contact Us</h2>
             <p><?php echo get_field('contactpage_text', 'option'); ?></p>
             <?php $removethese = array("(", " ", ")", "-"); ?>
@@ -9,7 +9,7 @@
                 <li><strong>Email:</strong> <a href="mailto:<?php echo get_field('primary_email', 'option'); ?>"><?php echo get_field('primary_email', 'option'); ?></a></li>
             </ul>
         </div><!-- col -->
-        <div class="col-lg-6 col-xl-7 pl-lg-2 pl-xl-3">
+        <div class="col-xl-7 pl-xl-3">
             <?php echo do_shortcode('[gravityform id="1" title="false" description="false"]'); ?>
         </div><!-- col -->
     </div><!-- row -->
